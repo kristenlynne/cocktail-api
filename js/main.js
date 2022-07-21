88f3e7afdab33a36cd99b828e9a7f8786069b757
Kristen Smith <kristennlynne@gmail.com>
2022-07-21 11:03:39 -0700

    added error message for invalid searches

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 /* To Do:
-- create function to get random drink and display it on the DOM
+x create function to get random drink and display it on the DOM
 - display random drinks in a carousel
 - create function to be able to search by ingredient name
-- create error message to display in dom when there's an invalid search
-- style the CSS
+x create error message to display in dom when there's an invalid search
+x style the CSS
 */
 
+/* -- Event Listeners -- */
 document.addEventListener('DOMContentLoaded', getDrink); 
-document.querySelector('button').addEventListener('click', getDrink) 
-document.querySelector('.randomButton').addEventListener('click', getRandomDrink)
+document.querySelector('.searchButton').addEventListener('click', getDrink);
+document.querySelector('.randomButton').addEventListener('click', getRandomDrink);
 
 /* -- displays drinks in DOM that meet parameters from input -- */
 async function getDrink() {
@@ -20,16 +21,25 @@ async function getDrink() {
     const data = await res.json();
       console.log(data);
       resetDOM()
-      data.drinks.forEach(drink => {
-        console.log(drink);
-        addToDOM(drink)
-      })
+      if (data.drinks == null) {
+        console.log('invalid search')
+        const section = document.createElement('section');
+        section.classList.add('error')
+        section.innerHTML = `<p>hmm... we don't know what that drink is. try another search.</p>`;
+        document.querySelector('#cocktails').appendChild(section)
+      } else {
+          data.drinks.forEach(drink => {
+            console.log(drink);
+            addToDOM(drink)
+        })
+      }
   }
   catch (err) {
     console.log(`Error: ${err}`)
   }
 }
 
+/* -- displays random drink in DOM when click on button -- */
 async function getRandomDrink(){
   const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`
 
@@ -48,6 +58,7 @@ async function getRandomDrink(){
   }
 }
 
+/* -- removes preloaded drinks from DOM -- */
 function resetDOM() {
     const cocktails = document.getElementById('cocktails');
 
@@ -55,6 +66,7 @@ function resetDOM() {
         cocktails.removeChild(cocktails.firstChild)
 }
 
+/* -- adds data from input to DOM and creates HTML elements --*/
 function addToDOM(drink) {
     const section = document.createElement('section');
     section.classList.add('cocktail')
@@ -73,6 +85,7 @@ function addToDOM(drink) {
     document.querySelector('#cocktails').appendChild(section)
 }
 
+/* -- loops through ingredient and measurement data and creates li elements -- */
 function getIngredients(drink) {
   let str = ''
   for (let i = 1; i < 16; i++) {
